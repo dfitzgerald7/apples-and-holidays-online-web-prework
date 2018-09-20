cbec7078a14e3edc96d0099979f50c88fb05b522
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-20 18:05:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     holidays.each do |holiday, supplies|
       holiday_array = holiday.split("_").collect {|word| word.to_s.capitalize}.join(" ")
-      holiday_array
+      
       holiday_string = ""
       holiday_string += " #{holiday.to_s.capitalize}: "
       
