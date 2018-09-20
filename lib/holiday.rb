b2230d6d35f57a730961da411e2f8e21feca6e60
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-20 18:01:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,8 @@ def all_supplies_in_holidays(holiday_hash)
       holiday_string = ""
       holiday_string += " #{holiday.to_s.capitalize}: "
       binding.pry
-      supplies.join(", ")
+      
+      holiday_string += supplies.join(", ")
     end 
     holiday_string[0...-1]
   end 
