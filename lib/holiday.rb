f08cf06edb136eb9920d4d8c2f299138065bccb4
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-20 18:03:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize + ":"
     
     holidays.each do |holiday, supplies|
-      holiday.split("_")
+      holiday_array = holiday.split("_")
+      
       holiday_string = ""
       holiday_string += " #{holiday.to_s.capitalize}: "
       
